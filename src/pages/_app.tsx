4c20dc8ab331f7014645c4a6b3eb88952a64fd29
vlaier <piotrzielu.1996@gmail.com>
2023-04-24 19:57:35 +0200

    feat: add Authentication Context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import Navbar from '@/components/Navbar';
+import { UserContextProvider } from '@/components/UserContext';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <UserContextProvider>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContextProvider>
   );
 }
