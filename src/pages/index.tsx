ba188d16d89a64658a37ba03abd4cd2e4daccde8
vlaier <piotrzielu.1996@gmail.com>
2023-04-24 19:58:42 +0200

    feat: wrap inside main tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import toast from 'react-hot-toast';
 
 export default function Home() {
   return (
-    <>
+    <main>
       <button onClick={() => toast.success('hello toast!')}>Toast Me</button>{' '}
       <Loader show={true} />
-    </>
+    </main>
   );
 }
