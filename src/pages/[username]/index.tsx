ab9dd35dfcbfbf97c1016dec1825163ae32c437b
vlaier <piotrzielu.1996@gmail.com>
2023-04-29 18:34:53 +0200

    feat: display user info and post on user page

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -1,7 +1,58 @@
-export default function UserPage() {
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import PostFeed from '@/components/PostFeed';
+import UserProfile from '@/components/UserProfile';
+import { firestore, getUserWithUsername, postToJSON } from '@/lib/firebase';
+import {
+  DocumentData,
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from 'firebase/firestore';
+
+export default function UserPage({
+  user,
+  posts,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <main>
-      <h1>user</h1>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </main>
   );
+}
+
+export async function getServerSideProps({
+  query: urlQuery,
+}: GetServerSidePropsContext) {
+  const username = urlQuery.username;
+  if (typeof username !== 'string') {
+    return {
+      notFound: true,
+    };
+  }
+  const userDoc = await getUserWithUsername(username);
+  let user = null;
+  let posts: DocumentData[] | null = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = query(
+      collection(firestore, userDoc.ref.path, 'posts'),
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'),
+      limit(5)
+    );
+    posts = (await getDocs(postsQuery)).docs.map((post) => postToJSON(post));
+  }
+  if (!user) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { user, posts },
+  };
 }
