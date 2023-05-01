45fb7f03f6ef526d4bb77f0fe863a3c8b8e26acf
vlaier <piotrzielu.1996@gmail.com>
2023-05-01 22:31:13 +0200

    feat: add AdminPage content

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,40 @@
+import Metatags from '@/components/Metatags';
+import AuthCheck from '@/components/AuthCheck';
+import { collection, orderBy, query } from 'firebase/firestore';
+import { auth, firestore } from '@/lib/firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import PostFeed from '@/components/PostFeed';
+import { PostData } from '@/index';
 export default function AdminPage() {
   return (
     <main>
-      <h1>Admin Page</h1>
+      <Metatags title="admin page" description="admin page" />
+      <AuthCheck>
+        <PostList />
+        <CreateNewPost />
+      </AuthCheck>
     </main>
   );
 }
+const PostList = () => {
+  const ref = collection(
+    firestore,
+    'users',
+    auth.currentUser?.uid as string,
+    'posts'
+  );
+  const postQuery = query(ref, orderBy('createdAt'));
+  const [querySnapshot] = useCollection(postQuery);
+  const posts: PostData[] | undefined = querySnapshot?.docs.map((doc) =>
+    doc.data()
+  );
+  return (
+    <>
+      <h1>Manage your Posts</h1>
+      <PostFeed posts={posts} admin={true} />
+    </>
+  );
+};
+const CreateNewPost = () => {
+  return <div>Create New Post</div>;
+};
